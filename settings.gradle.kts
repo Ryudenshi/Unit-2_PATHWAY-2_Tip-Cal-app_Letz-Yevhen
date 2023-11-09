a0480fdbc7539ecbd12c5c95cbb6e9f581a2124b
Ryudenshi <skarydog17@gmail.com>
2023-11-09 12:46:39 +0200

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Unit 2_PATHWAY 2_Tip Cal app_Letz Yevhen"
+include(":app")
+ 
